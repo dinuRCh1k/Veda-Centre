6704341db40ddbdb490f8492852f7c8ee38165f1
Dinur <sdinur2@gmail.com>
2024-02-21 23:07:33 +0500

    сверстал страницу со статьями и исправил ссылки в футере и в хедере

--- js/index.js
+++ js/index.js
@@ -1,17 +1,14 @@
 const checkbox = document.getElementById('hamburger');
 const main = document.querySelector('main');
-const cd = document.getElementById('cd');
 const lineheader = document.getElementById('line-header');
 const footer = document.querySelector('footer');
 checkbox.addEventListener('change', function() {
     if (checkbox.checked) {
         main.style.display = 'none';
-        cd.style.display = 'none';
         footer.style.display = 'none';
         lineheader.style.display = 'none';
     } else {
         main.style.display = '';
-        cd.style.display = '';
         footer.style.display = '';
         lineheader.style.display = '';
     }
